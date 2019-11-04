fd902fbd279bf8a9017ec9e44b35340258683ccf
Sebastian Trujillo <juanstt99@gmail.com>
2019-11-04 10:38:44 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class UserController extends Controller
 {
-    public function signup($email, $password){
-    	return response()->json([
+    public function signup(Request $request){
+    	$response = [
     		'code' => 200,
     		'message' => 'successful',
     		'ok' => true
-    	]);
+    	];
+
+    	try{
+    		$user = new User;
+            $user->email = $request->email;
+            $user->password = $request->password;
+            $user->save();
+    	} catch (Illuminate\Database\QueryException $e){
+    		$errorCode = $e->errorInfo[1];
+
+            if($errorCode == 1062){
+                $response = [
+                    'ok' => false,
+                    'code' => $errorCode
+                ];
+            }
+    	}
+
+    	return response()->json($response);
     }
 
     public function login($email, $password){
