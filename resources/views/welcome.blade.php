89501d9d562be6138e068b79a30961e24309a194
Sebastian Trujillo <juanstt99@gmail.com>
2019-11-04 08:40:07 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 88888
+                    Laravel 1544
                 </div>
 
                 <div class="links">
