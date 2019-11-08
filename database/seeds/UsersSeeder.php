bfa93f410e43fe10ad4a163373242b420d0f0ff6
Sebastian Trujillo <juanstt99@gmail.com>
2019-11-08 14:16:13 -0500

    Update UsersSeeder.php

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -7,52 +7,52 @@ class UsersSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 1,
             'name' => 'Juan Camilo',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 1',
+            'password' => 'ABC1'
         ]);
 
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 2,
             'name' => 'Camilo',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 2',
+            'password' => 'ABC2'
         ]);
 
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 3,
             'name' => 'Pablo',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 3',
+            'password' => 'ABC3'
         ]);
 
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 4,
             'name' => 'Juan Pablo',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 4',
+            'password' => 'ABC4'
         ]);
 
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 5,
             'name' => 'Jose',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 5',
+            'password' => 'ABC5'
         ]);
 
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 6,
             'name' => 'Juan Jose',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 6',
+            'password' => 'ABC6'
         ]);
 
         DB::table('users')->insert([
-            'doc' => $i,
+            'doc' => 7,
             'name' => 'Luis Jose',
-            'email' => 'email '.$i,
-            'password' => 'ABC'.$i
+            'email' => 'email 7',
+            'password' => 'ABC7'
         ]);
     }
 }
