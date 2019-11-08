0d72951154b878f881d7b02a7c8cebbbe862a860
Sebastian Trujillo <juanstt99@gmail.com>
2019-11-08 10:46:23 -0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -52,7 +52,7 @@ class ProjectController extends Controller
     	}*/
 
         return response()->json([
-        	"data" => $$request->name
+        	"data" => $request->name
         ]);
     }
 }
