5bf6f6276dab31a0c5360f7db7cfcf5e84a47125
Sebastian Trujillo <juanstt99@gmail.com>
2019-11-10 07:05:41 -0500

    seeder

--- database/seeds/ProjectUserSeeder.php
+++ database/seeds/ProjectUserSeeder.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Database\Seeder;
 
-class ProjectsUsersSeeder extends Seeder
+class ProjectUserSeeder extends Seeder
 {
     public function run()
     {
         for($i = 1; $i < 6; $i++){
-	        DB::table('users_projects')->insert([
+	        DB::table('project_user')->insert([
 	            'project_id' => $i,
 	            'user_doc' => 1,
 	        ]);
