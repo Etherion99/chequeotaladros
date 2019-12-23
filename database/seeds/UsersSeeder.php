eabfb23e7d81a579836d4b5a1a08f82696680056
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-23 17:28:22 -0500

    Update UsersSeeder.php

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -10,49 +10,49 @@ class UsersSeeder extends Seeder
             'doc' => 1,
             'name' => 'Juan Camilo',
             'email' => 'email 1',
-            'password' => 'ABC1'
+            'password' => Hash::make('ABC1')
         ]);
 
         DB::table('users')->insert([
             'doc' => 2,
             'name' => 'Camilo',
             'email' => 'email 2',
-            'password' => 'ABC2'
+            'password' => Hash::make('ABC2')
         ]);
 
         DB::table('users')->insert([
             'doc' => 3,
             'name' => 'Pablo',
             'email' => 'email 3',
-            'password' => 'ABC3'
+            'password' => Hash::make('ABC3')
         ]);
 
         DB::table('users')->insert([
             'doc' => 4,
             'name' => 'Juan Pablo',
             'email' => 'email 4',
-            'password' => 'ABC4'
+            'password' => Hash::make('ABC4')
         ]);
 
         DB::table('users')->insert([
             'doc' => 5,
             'name' => 'Jose',
             'email' => 'email 5',
-            'password' => 'ABC5'
+            'password' => Hash::make('ABC5')
         ]);
 
         DB::table('users')->insert([
             'doc' => 6,
             'name' => 'Juan Jose',
             'email' => 'email 6',
-            'password' => 'ABC6'
+            'password' => Hash::make('ABC6')
         ]);
 
         DB::table('users')->insert([
             'doc' => 7,
             'name' => 'Luis Jose',
             'email' => 'email 7',
-            'password' => 'ABC7'
+            'password' => Hash::make('ABC7')
         ]);
     }
 }
