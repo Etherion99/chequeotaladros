03d76c716fdc201c14d44c6cf446bab5e5f641ed
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-29 08:48:30 -0500

    Update ReviewsSeeder.php

--- database/seeds/ReviewsSeeder.php
+++ database/seeds/ReviewsSeeder.php
@@ -7,23 +7,23 @@ class ReviewsSeeder extends Seeder
     public function run()
     {
         for($i = 1; $i < 6; $i++){
-	        DB::table('projects')->insert([
-	            'name' => 'nombre '.$i,
-	            'creator' => $i,
+	        DB::table('reviews')->insert([
+	            'project_id' => $i,
+	            'creator_doc' => 1,
 	        ]);
     	}
 
     	for($i = 1; $i < 6; $i++){
-	        DB::table('projects')->insert([
-	            'name' => 'nombre '.$i,
-	            'creator' => $i,
+	        DB::table('reviews')->insert([
+	            'project_id' => $i,
+	            'creator_doc' => 2,
 	        ]);
     	}
 
     	for($i = 1; $i < 6; $i++){
-	        DB::table('projects')->insert([
-	            'name' => 'nombre '.$i,
-	            'creator' => $i,
+	        DB::table('reviews')->insert([
+	            'project_id' => $i,
+	            'creator_doc' => 3,
 	        ]);
     	}
     }
