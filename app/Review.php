9c30b068b836265d9e3a47d2cbfdafda2d2ba515
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-28 23:14:58 -0500

    Update Review.php

--- app/Review.php
+++ app/Review.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+	protected $fillable = [
+        'creator', 'project_id'
+    ];
+
     public function reviewItemRecords(){
     	return $this->hasMany('App\ReviewitemRecord');
     }
