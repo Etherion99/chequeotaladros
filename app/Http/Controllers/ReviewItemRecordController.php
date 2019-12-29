5e2516e1d5ff1d55f0723a6546a7a68978870a26
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-29 17:52:23 -0500

    review items records by review id

--- app/Http/Controllers/ReviewItemRecordController.php
+++ app/Http/Controllers/ReviewItemRecordController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ReviewItemRecord;
 
 class ReviewItemRecordController extends Controller
 {
-    //
+    public function showByReview($id){
+    	ReviewItemRecord::where('review_id', $id)->get();
+    }
 }
--- app/ReviewItemRecord.php
+++ app/ReviewItemRecord.php
@@ -17,8 +17,13 @@ class ReviewItemRecord extends Model
     }
 
     public function review(){
-    	return $this->belongsTo('App\Review');
+    	return $this->belongsTo('App\Review', 'review_id');
     }
+
+    public function item(){
+        return $this->belongsTo('App\ReviewItem', 'item_id');
+    }
+
     public function ReviewItemPhotos(){
     	return $this->hasMany('App\ReviewItemPhoto', 'review_item_record');
     }
