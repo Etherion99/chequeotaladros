949e007f33b07dc7d96b286cd571fb3d1fe8bbed
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-29 21:50:36 -0500

    category db

--- app/ReviewItem.php
+++ app/ReviewItem.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReviewItem extends Model
 {
-    public function reviewCategory(){
-    	return $this->belongsTo('App\ReviewCategory', 'category');
+    public function category(){
+    	return $this->belongsTo('App\ReviewCategory', 'category_id');
     }
 
     public function ReviewItemRecords(){
--- database/migrations/2019_12_22_192612_create_review_items_table.php
+++ database/migrations/2019_12_22_192612_create_review_items_table.php
@@ -11,7 +11,7 @@ class CreateReviewItemsTable extends Migration
         Schema::create('review_items', function (Blueprint $table) {
             $table->smallIncrements('id');
             $table->string('name', 350);
-            $table->tinyInteger('category')->unsigned();
+            $table->tinyInteger('category_id')->unsigned();
 
             $table->foreign('category')->references('id')->on('review_categories')->onDelete('cascade');
         });
