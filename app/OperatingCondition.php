b0870a8c8878521fd47139ca511d9c830b5bb5ba
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-30 15:14:37 -0500

    Update OperatingCondition.php

--- app/OperatingCondition.php
+++ app/OperatingCondition.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class OperatingCondition extends Model
 {
+	protected $fillable = [
+        'type',
+        'value'
+    ];
+
     public function record(){
     	return $this->belongsTo('App\ReviewItemRecord', 'record_id');
     }
