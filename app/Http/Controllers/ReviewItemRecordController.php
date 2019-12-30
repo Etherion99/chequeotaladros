3338ec030dbe0ed768511644e2fd49b36f401906
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-29 22:59:44 -0500

    Update ReviewItemRecordController.php

--- app/Http/Controllers/ReviewItemRecordController.php
+++ app/Http/Controllers/ReviewItemRecordController.php
@@ -4,10 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ReviewItemRecord;
+use App\ReviewCategory;
 
 class ReviewItemRecordController extends Controller
 {
     public function showByReview($id){
-    	return response()->json(ReviewItemRecord::where('review_id', $id)->with('item.category')->get());
+    	$categories = ReviewCategory::with('items')->get();
+
+    	$categoryRecords = array();
+
+    	foreach($categories as $category){
+    		$records = array();
+
+    		foreach($category['items'] as $item){
+    			$record = ReviewItemRecord::where('review_id', $id)
+    				->where('item_id', $item['id']
+    				->get());
+
+    			array_push($records, $record);
+    		}
+
+    		array_push($categoryRecords, array(
+    			'id' => ,
+    			'items' => $records
+    		));
+    	}
+
+    	//ReviewItemRecord::where('review_id', $id)->with('item.category')->get();
+
+    	return response()->json($categoryRecords);
     }
 }
