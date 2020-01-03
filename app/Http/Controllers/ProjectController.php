e84a3ad7b81b43468e65330658f8a8755e3a96ca
Sebastian Trujillo <juanstt99@gmail.com>
2020-01-03 13:33:11 -0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -24,7 +24,7 @@ class ProjectController extends Controller
         else
             $projects = User::find($request->user_doc)->share_projects()->get();
 
-        return response()->json($request->own);
+        return response()->json($request->own."dd");
     }
 
     public function store(Request $request){
