690109fbac41bc44c168d4d9e45b5cf484a839ec
Sebastian Trujillo <juanstt99@gmail.com>
2020-01-03 11:17:12 -0500

    Update 2019_11_09_130632_create_user_project_table.php

--- database/migrations/2019_11_09_130632_create_user_project_table.php
+++ database/migrations/2019_11_09_130632_create_user_project_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersProjectsTable extends Migration
+class CreateUserProjectTable extends Migration
 {
     public function up()
     {
