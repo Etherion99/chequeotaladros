84dc5835da1afb51f4819a563a32722d3501690a
Sebastian Trujillo <juanstt99@gmail.com>
2020-01-03 11:37:08 -0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -17,7 +17,7 @@ class ProjectController extends Controller
             $projects = Project::where('creator_doc', $request->user_doc)
                         ->with('creator_user')->get();
         else
-            $projects = User::find($request->user_doc)->projects();
+            $projects = User::find($request->user_doc)->projects()->get();
 
         return response()->json($projects);
     }
