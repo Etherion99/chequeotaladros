ac36e1bed4b85c15ab37adfbfc642d2024fd73ed
Sebastian Trujillo <juanstt99@gmail.com>
2020-01-14 17:29:26 -0500

    Update ReviewItemPhotoController.php

--- app/Http/Controllers/ReviewItemPhotoController.php
+++ app/Http/Controllers/ReviewItemPhotoController.php
@@ -11,31 +11,17 @@ class ReviewItemPhotoController extends Controller
     }
 
     public function store(Request $request){
-    	/*if($response['ok'] && !is_null($request->file('photo'))){
-    		$photo = $request->file('photo');
-	        $name = $user->id.'.'.$photo->getClientOriginalExtension();
-	        $path = public_path('/images/profiles');
-
-	        if(!$photo->move($path, $name)){
-	        	$response['message'] = "Error al guardar imagen";
-	        }
-    	}*/
-
     	$response = [
             'code' => 4,
-            'message' => json_encode($request->photo),
+            'message' => json_encode($request->items),
             'ok' => true
         ];
 
-        $m = "hola ";
-
         foreach($request->file('photo') as $photo){
             if(!is_null($photo)){
-                $m .= $photo->getClientOriginalExtension()."*  *";
+               
             }
         }
-
-        $response['message'] = $m;
 
     	return response()->json($response);
     }
