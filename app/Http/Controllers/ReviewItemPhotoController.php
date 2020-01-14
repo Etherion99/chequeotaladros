90c8baf6ce3eefdddd5f3370a5fb718f022f6b54
Sebastian Trujillo <juanstt99@gmail.com>
2020-01-14 18:50:41 -0500

    Update ReviewItemPhotoController.php

--- app/Http/Controllers/ReviewItemPhotoController.php
+++ app/Http/Controllers/ReviewItemPhotoController.php
@@ -40,7 +40,7 @@ class ReviewItemPhotoController extends Controller
                 }
             }
 
-            $response['message'] = $request->all();
+            $response['message'] = json_encode($request->all());
         } catch (Exception $e){
             $response = [
                 'code' => $e->errorInfo[1],
