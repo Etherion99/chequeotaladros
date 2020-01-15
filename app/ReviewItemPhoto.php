4aa3c0f44d3d88abdf247cdc84187559b2805cc6
Sebastian Trujillo <juanstt99@gmail.com>
2020-01-14 19:35:14 -0500

    Update ReviewItemPhoto.php

--- app/ReviewItemPhoto.php
+++ app/ReviewItemPhoto.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReviewItemPhoto extends Model
 {
+	protected $fillable = [
+        'review_item_record',
+        'extension'
+    ];
+
     public function ReviewItemRecord(){
     	return $this->belongsTo('App\ReviewItemRecord', 'review_item_record');
     }
