887fddcf35546a25e15dafe26f0f2147973c8004
Sebastian Trujillo <juanstt99@gmail.com>
2020-04-23 18:07:02 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -148,6 +148,12 @@ class UserController extends Controller
     public function verifyCode($doc, $code){
         $user = User::select('code')->where('doc', $doc)->first();
 
+        $response = array('status' => 'error');
+
+
+        if($code == $user->code)
+            $repsonse['status'] = 'ok';
+
         return response()->json($user);
     }
 }
