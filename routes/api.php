1a1ff897ee72f2cc85a3825d34e5170e38314dc6
Sebastian Trujillo <juanstt99@gmail.com>
2020-04-23 17:23:11 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ Route::post('user/login', 'UserController@login');
 Route::get('user/show/{doc}', 'UserController@show');
 Route::post('user/update', 'UserController@update');
 Route::get('user/search/{text}', 'UserController@search');
+Route::get('user/sendResetPassMail/{doc}', 'UserController@sendCode');
 
 Route::get('project/view/{id}', 'ProjectController@show');
 Route::get('project/all', 'ProjectController@showAll');
