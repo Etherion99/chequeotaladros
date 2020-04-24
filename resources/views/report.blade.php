c68c0421e0c9500afbaf4e8ef76af557d64cb922
Sebastian Trujillo <juanstt99@gmail.com>
2020-04-24 12:37:59 -0500

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,14 +1,17 @@
-<style>
-	.column{
-		width: 50%;
-		display: inline-block;
-	}
-</style>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
 <h3><center>RIG ACCEPTANCE APP</center></h3>
 <br><br>
 <h5><center>REPORTE</center></h5>
 <br><br>
+<div class="row">
+	<div class="col-6">
+		f
+	</div>
+	<div class="col-6">
+		ff
+	</div>
+</div>
 <div class="column">
 	<p>Empresa operadora:<u>     kk                   </u></p>
 	<p>Responsable:<u>                        </u></p>
