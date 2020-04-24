dc65e02e060e76540f723d7773ac45bb94faf388
Sebastian Trujillo <juanstt99@gmail.com>
2020-04-24 13:15:32 -0500

    Update ReviewItemRecordController.php

--- app/Http/Controllers/ReviewItemRecordController.php
+++ app/Http/Controllers/ReviewItemRecordController.php
@@ -48,6 +48,6 @@ class ReviewItemRecordController extends Controller
 
     	//ReviewItemRecord::where('review_id', $id)->with('item.category')->get();
 
-    	return response()->json($categoryRecords);
+    	return response()->json($categories);
     }
 }
